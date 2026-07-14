e199e7051bc266bace67be4d6bc363e1db7e52ff
riefky <2006085@itg.ac.id>
2026-07-14 13:50:08 +0700

    add template layout and add breeze

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<header class="mb-3">
+    <a href="#" class="burger-btn d-block d-xl-none">
+        <i class="bi bi-justify fs-3"></i>
+    </a>
+</header>
+
+<div class="page-heading">
+    <h3>Tasks</h3>
+</div>
+
+<div class="page-content">
+    task
+</div>
+
+@endsection
