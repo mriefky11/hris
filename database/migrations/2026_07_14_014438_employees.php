db08f86c80f040e4f2c443b89359e55397ce1a18
riefky <2006085@itg.ac.id>
2026-07-14 08:53:11 +0700

    add migration database hris

---
+++ database/migrations/2026_07_14_014438_employees.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('fullname');
+            $table->string('email')->unique();
+            $table->string('phone_number');
+            $table->text('address');
+            $table->timestamp('birth_date');
+            $table->timestamp('hire_date');
+            $table->foreignId('department_id')->constrained('departments');
+            $table->foreignId('role_id')->constrained('roles');
+            $table->string('status');
+            $table->decimal('salary', 10, 2);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
